127c5167b08f61709b5da35e77f08dc1aa6d9aab
Warrgun <131681381+Warrgun@users.noreply.github.com>
2024-05-28 16:26:58 +0200

    css

---
+++ watch-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ClockApp from './ClockApp'
+
+function App() {
+
+
+  return (
+    <>
+        <ClockApp/>
+    </>
+  )
+}
+
+export default App
---
+++ watch-app/src/ClockApp.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+
+const ClockApp = ()=>{
+
+    return(
+        <div className="container">
+            <div className="clock-container">
+                <h1 className="clock">00:00:00</h1>
+            </div>    
+        </div>
+    );
+};
+
+export default ClockApp
