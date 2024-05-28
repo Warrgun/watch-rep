6cecefe7df8c3f6bf805c205f59c3a7e76c4588e
Warrgun <131681381+Warrgun@users.noreply.github.com>
2024-05-28 18:28:28 +0200

    finished

--- watch-app/src/ClockApp.jsx
+++ watch-app/src/ClockApp.jsx
@@ -1,11 +1,38 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 const ClockApp = ()=>{
+    const [time, setTime] = useState(new Date);
+    const [isMilitery, setIsMilitery] = useState(false);
+
+    useEffect(() =>{
+        const intervalId = setInterval(() => setTime(new Date) ,1000);
+
+        return () => clearInterval(intervalId);
+    },[])
+
+    let hours = time.getHours();
+    const minutes = time.getMinutes();
+    const secounds = time.getSeconds();
+
+    const addZero = (number) => number<10? `0${number}`:number;
+
+    const handleTimeMilitery = `${addZero(hours)}:${addZero(minutes)}:${addZero(secounds)}`;
+
+    const handleTime = () =>{
+        const noon = () => hours >= 12?"PM":"AM"; 
+        const digits = () => hours % 12 !==0? hours%12: 12; 
+
+        return `${addZero(digits())}:${addZero(minutes)}:${addZero(secounds)} ${noon()}`
+    }
+
+    const handleIsMilitery = () => setIsMilitery(!isMilitery);
+
+    const changeClock = () => isMilitery? handleTimeMilitery :handleTime();
 
     return(
         <div className="container">
             <div className="clock-container">
-                <h1 className="clock">00:00:00</h1>
+                <h1 className="clock" onClick={handleIsMilitery}>{changeClock()}</h1>
             </div>    
         </div>
     );
